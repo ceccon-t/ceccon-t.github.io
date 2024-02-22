3b02e260910271795f543a03552bc35309932c39
Tiago Ceccon <ceccon.t@hotmail.com>
2024-02-21 23:17:21 -0300

    Compute current year dynamically

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 // navbar highlight on scrolling
 $('body').scrollspy({target: 'nav'});
+
+// dynamic initializations
+document.getElementById('current-year').textContent = new Date().getFullYear();
